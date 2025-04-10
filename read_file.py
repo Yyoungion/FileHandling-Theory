0e4a3201b7c0c96b3965b4c8af513bf48dc91b41
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-04-10 14:53:24 +1000

    Finished read_file.py

--- read_file.py
+++ read_file.py
@@ -1,5 +1,8 @@
 # TODO: Open the file "students.txt" in read mode
+with open("students.txt", "r") as file:  # 'r' mode opens the file for reading
 
     # TODO: Read the file content (use the correct file method)
+    content = file.read()  # Use the 'read()' method to get the entire content as a string
 
 # TODO: Print the content of the file
+    print(content)
